f7f704326c2d9c6a12c2fc6215c0fae561226405
luciosilva25 <luciogeronimosilva@gmail.com>
2022-09-05 10:36:38 -0300

    cd

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text,View, StyleSheet, Button,Image } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
+import logo from './assets/logo.png'; 
 
 export default function App() {
+  const video = React.useRef(null);
+  const video1 = React.useRef(null);
+  const [status, setStatus] = React.useState({});
+  const [status1, setStatus1] = React.useState({});
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Image source={logo}style={styles.logo} />
+        <Text style={styles.titleText}> Big Buck Bunny </Text>
+       
+     
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
+
+      <Video
+        ref={video1}
+        style={styles.video}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status1 => setStatus1(() => status1)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status1.Play? 'Pause' : 'Play'}
+          onPress={() =>
+            status1.Play ? video1.current.pauseAsync() : video1.current.playAsync()
+          }
+        />
+      </View>
+
+      
+      
+      
     </View>
   );
 }
@@ -13,8 +64,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    alignItems:'center'
   },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 25,
+
+  },
+  logo:{ 
+    width: 200,
+    height: 54,
+   
+   }
+  
 });
